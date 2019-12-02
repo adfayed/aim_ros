b34ab7ef18ce1b007a0cd0dc3886e28f83e850ea
Ahmed Fayed <ADFayed@Hotmail.com>
2019-12-01 20:19:08 -0800

    Policy 0 and 2 working. Minor fixes needed on ACC

--- src/aim/src/carManager.py
+++ src/aim/src/carManager.py
@@ -42,61 +42,150 @@ class car:
 		self.max_lateral_g = max_lateral_g
 		self.reservation = reservation
 
+	# This is a function to check if car has cleared its lane (rearmost edge)
+	def _check_for_exit(self, t_index):
+		if self.lane_id == 6 or self.lane_id == 7 or self.lane_id == 8:
+			# South lanes
+			return True if ((self.y[t_index] - self.length/2) > dMax) else False # - self.length/2
 
-	def _update(self, curr_t_index, follow_car = None , f_curr_t_index = None):
-		if self.reservation.any() == True:
-			# Follow x, y, heading, vel given by IM
-			pass
-		else:
-			self.heading[curr_t_index] = self.heading[curr_t_index - 1]
-			if self.lane_id == 6 or self.lane_id == 7 or self.lane_id == 8:
-				# Position calculations for South lanes
-				self.y[curr_t_index] = self.y[curr_t_index - 1] - (0.5*self.acc[curr_t_index - 1]*(timestep_size**2)) + self.vel[curr_t_index - 1]*timestep_size
-				self.x[curr_t_index] = self.x[curr_t_index - 1]
-				stopping_distance = (dMax - self.y[curr_t_index] - self.length/2)
-			elif self.lane_id == 3 or self.lane_id == 4 or self.lane_id == 5:
-				# Position calculations for East lanes
-				self.y[curr_t_index] = self.y[curr_t_index - 1]
-				self.x[curr_t_index] = self.x[curr_t_index - 1] + (0.5*self.acc[curr_t_index - 1]*(timestep_size**2)) - self.vel[curr_t_index - 1]*timestep_size
-				stopping_distance = (self.x[curr_t_index] - (dMax+6*lane_width) - self.length/2)
-			elif self.lane_id == 0 or self.lane_id == 1 or self.lane_id == 2:
-				# Position calculations for North lanes
-				self.x[curr_t_index] = self.x[curr_t_index - 1]
-				self.y[curr_t_index] = self.y[curr_t_index - 1] + (0.5*self.acc[curr_t_index - 1]*(timestep_size**2)) - self.vel[curr_t_index - 1]*timestep_size
-				stopping_distance = (self.y[curr_t_index] - (dMax+6*lane_width) - self.length/2)
-			elif self.lane_id == 9 or self.lane_id == 10 or self.lane_id == 11:
-				# Position calculations for West lanes
-				self.x[curr_t_index] = self.x[curr_t_index - 1] - (0.5*self.acc[curr_t_index - 1]*(timestep_size**2)) + self.vel[curr_t_index - 1]*timestep_size
-				self.y[curr_t_index] = self.y[curr_t_index - 1]
-				stopping_distance = (dMax - self.x[curr_t_index] - self.length/2)
-			self.vel[curr_t_index] = self.vel[curr_t_index - 1] + self.acc[curr_t_index - 1]*timestep_size
-			if follow_car is None:
-				self.acc[curr_t_index] = (-self.vel[curr_t_index - 1] / (2*stopping_distance/self.vel[curr_t_index - 1]))
+		elif self.lane_id == 3 or self.lane_id == 4 or self.lane_id == 5:
+			# East lanes
+			return True if ((self.x[t_index] + self.length/2) < (dMax+6*lane_width)) else False # + self.length/2
+
+		elif self.lane_id == 0 or self.lane_id == 1 or self.lane_id == 2:
+			# North lanes
+			return True if ((self.y[t_index] + self.length/2) < (dMax+6*lane_width)) else False # + self.length/2
+
+		elif self.lane_id == 9 or self.lane_id == 10 or self.lane_id == 11:
+			# West lanes
+			return True if ((self.x[t_index] - self.length/2) > dMax) else False # - self.length/2
+
+		#returns True if exited False otherwise
+
+	def _expected_vel(self, t_index, stopping_distance):
+		nominal_dist = dMax
+		nominal_decel = (-self.vel[0] / (2*dMax/self.vel[0]))
+		for i in range(0,t_index+1):
+			expected_vel = self.vel[0] + (nominal_decel*timestep_size * i)
+			nominal_dist = nominal_dist - expected_vel
+			if nominal_dist <= stopping_distance:
+				break
+		return expected_vel
+
+	def _update(self, t_index, follow_car = None , f_t_index = None):
+		self.heading[t_index] = self.heading[t_index - 1]
+		if self.lane_id == 6 or self.lane_id == 7 or self.lane_id == 8:
+			# Position calculations for South lanes
+			self.y[t_index] = self.y[t_index - 1] - (0.5*self.acc[t_index - 1]*(timestep_size**2)) + self.vel[t_index - 1]*timestep_size
+			self.x[t_index] = self.x[t_index - 1]
+			stopping_distance = (dMax - self.y[t_index] - self.length/2)
+		elif self.lane_id == 3 or self.lane_id == 4 or self.lane_id == 5:
+			# Position calculations for East lanes
+			self.y[t_index] = self.y[t_index - 1]
+			self.x[t_index] = self.x[t_index - 1] + (0.5*self.acc[t_index - 1]*(timestep_size**2)) - self.vel[t_index - 1]*timestep_size
+			stopping_distance = (self.x[t_index] - (dMax+6*lane_width) - self.length/2)
+		elif self.lane_id == 0 or self.lane_id == 1 or self.lane_id == 2:
+			# Position calculations for North lanes
+			self.x[t_index] = self.x[t_index - 1]
+			self.y[t_index] = self.y[t_index - 1] + (0.5*self.acc[t_index - 1]*(timestep_size**2)) - self.vel[t_index - 1]*timestep_size
+			stopping_distance = (self.y[t_index] - (dMax+6*lane_width) - self.length/2)
+		elif self.lane_id == 9 or self.lane_id == 10 or self.lane_id == 11:
+			# Position calculations for West lanes
+			self.x[t_index] = self.x[t_index - 1] - (0.5*self.acc[t_index - 1]*(timestep_size**2)) + self.vel[t_index - 1]*timestep_size
+			self.y[t_index] = self.y[t_index - 1]
+			stopping_distance = (dMax - self.x[t_index] - self.length/2)
+		self.vel[t_index] = self.vel[t_index - 1] + self.acc[t_index - 1]*timestep_size
+		nominal_decel = (-self.vel[0] / (2*dMax/self.vel[0]))
+		if follow_car is None:
+			# Slow down to the line or speed up to the line
+			if stopping_distance <= 1:
+				# DONE
+				self.acc[t_index] = 0
+				self.vel[t_index] = 0
 			else:
-				if self.lane_id == 6 or self.lane_id == 7 or self.lane_id == 8: # South lanes
-					car_gap = (follow_car.y[f_curr_t_index] - follow_car.length/2) - (self.y[curr_t_index] + self.length/2)
-				elif self.lane_id == 3 or self.lane_id == 4 or self.lane_id == 5: # East lanes
-					car_gap = (self.x[curr_t_index] - self.length/2 ) - (follow_car.x[f_curr_t_index] + follow_car.length/2)
-				elif self.lane_id == 0 or self.lane_id == 1 or self.lane_id == 2: # North lanes
-					car_gap = (self.y[curr_t_index] - self.length/2) - (follow_car.y[f_curr_t_index] + follow_car.length/2)
-				elif self.lane_id == 9 or self.lane_id == 10 or self.lane_id == 11: # West lanes
-					car_gap = (follow_car.x[f_curr_t_index] - follow_car.length/2) - (self.x[curr_t_index] + self.length/2)
-				self.acc[curr_t_index] = (follow_car.vel[f_curr_t_index - 1] - self.vel[curr_t_index - 1])/(2*(car_gap - dSafe)/self.vel[curr_t_index - 1])
-				self.following[curr_t_index] = True # For visualization purposes
-				if car_gap <= 1.5*dSafe:
-					try:
-						self.acc[curr_t_index] = self.acc[curr_t_index] - math.exp(1.5*dSafe - car_gap)
-					except OverflowError as err:
-						self.acc[curr_t_index] = self.acc[curr_t_index] - 5
-				if follow_car.reservation[f_curr_t_index] == True:
-					follow_acc = (follow_car.vel[f_curr_t_index - 1] - self.vel[curr_t_index - 1])/(2*(car_gap - dSafe)/self.vel[curr_t_index - 1])
-					stopping_acc = (-self.vel[curr_t_index - 1] / (2*stopping_distance/self.vel[curr_t_index - 1]))
-					self.acc[curr_t_index] = min(follow_acc, stopping_acc)
-					if stopping_acc < follow_acc:
-						self.following[curr_t_index] = False # For visualization purposes
-			if self.vel[curr_t_index] <= 2:
-				self.vel[curr_t_index] = 0
-				self.acc[curr_t_index] = 0
+				if self.vel[t_index] > 0:
+					# Slowdown to line
+					line_slowdown_acc = (-self.vel[t_index] / (2*stopping_distance/self.vel[t_index]))
+					if line_slowdown_acc <= nominal_decel:
+						self.acc[t_index] = line_slowdown_acc
+					else:
+						# Speed up to line
+						expected_vel = self._expected_vel(t_index, stopping_distance)
+						# Attempt to speed up to expected_vel in half the stopping distance left
+						line_speedup_acc = (expected_vel)
+						self.acc[t_index] = line_speedup_acc
+				else:
+					expected_vel = self._expected_vel(t_index, stopping_distance)
+					# Attempt to speed up to expected_vel in half the stopping distance left
+					line_speedup_acc = (expected_vel)
+					self.acc[t_index] = line_speedup_acc
+		else:
+			# FOLLOW CAR EXISTS
+			if self.lane_id == 6 or self.lane_id == 7 or self.lane_id == 8: # South lanes
+				car_gap = (follow_car.y[f_t_index] - follow_car.length/2) - (self.y[t_index] + self.length/2)
+			elif self.lane_id == 3 or self.lane_id == 4 or self.lane_id == 5: # East lanes
+				car_gap = (self.x[t_index] - self.length/2 ) - (follow_car.x[f_t_index] + follow_car.length/2)
+			elif self.lane_id == 0 or self.lane_id == 1 or self.lane_id == 2: # North lanes
+				car_gap = (self.y[t_index] - self.length/2) - (follow_car.y[f_t_index] + follow_car.length/2)
+			elif self.lane_id == 9 or self.lane_id == 10 or self.lane_id == 11: # West lanes
+				car_gap = (follow_car.x[f_t_index] - follow_car.length/2) - (self.x[t_index] + self.length/2)
+			if stopping_distance <= 1:
+				# DONE
+				self.acc[t_index] = 0
+				self.vel[t_index] = 0	
+				print("Shouldn't have a follow car.")
+			else:
+				if self.vel[t_index] > 0:
+					if car_gap - dSafe >= 1:
+						follow_acc = (follow_car.vel[f_t_index] - self.vel[t_index])/(2*(car_gap - dSafe)/self.vel[t_index])
+					else:
+						follow_acc = follow_car.acc[f_t_index]
+						follow_vel = follow_car.vel[f_t_index]
+					line_slowdown_acc = (-self.vel[t_index] / (2*stopping_distance/self.vel[t_index]))
+					if follow_acc <= nominal_decel or line_slowdown_acc <= nominal_decel:
+						# Slow Down neccessary
+						if follow_acc <= line_slowdown_acc:
+							# Slow to follow car
+							self.acc[t_index] = follow_acc
+							if car_gap - dSafe < 1:
+								self.vel[t_index] = follow_vel
+							self.following[t_index] = True
+						else:
+							# Slow to line
+							self.acc[t_index] = line_slowdown_acc
+					else:
+						# Speed up
+						expected_vel = self._expected_vel(t_index, stopping_distance)
+						# Attempt to speed up to expected_vel in half the stopping distance left
+						line_speedup_acc = (expected_vel)
+						if follow_acc <= line_speedup_acc:
+							self.acc[t_index] = follow_acc
+							if car_gap - dSafe < 1:
+								self.vel[t_index] = follow_vel
+							self.following[t_index] = True
+						else:
+							self.acc[t_index] = line_speedup_acc						
+				else:
+					# Take follow_car's acc and vel if they are less than line's
+					expected_vel = self._expected_vel(t_index, stopping_distance)
+					# Attempt to speed up to expected_vel in half the stopping distance left
+					line_speedup_acc = (expected_vel)
+					if car_gap - dSafe >= 1:
+						follow_acc = follow_car.vel[f_t_index]/(2*(car_gap - dSafe)/(car_gap - dSafe))
+					else:
+						follow_acc = follow_car.acc[f_t_index]
+						follow_vel = follow_car.vel[f_t_index]
+					if follow_acc <= line_speedup_acc:
+						self.acc[t_index] = follow_acc
+						if car_gap - dSafe < 1:
+							self.vel[t_index] = follow_vel
+						self.following[t_index] = True
+					else:
+						self.acc[t_index] = line_speedup_acc
+
+		# if self.vel[t_index] <= 2:
+		# 	self.vel[t_index] = 0
+		# 	self.acc[t_index] = 0
 
 
 class carManager:
@@ -110,7 +199,7 @@ class carManager:
 				continue
 			else:
 				curr_t_index = None
-				f_curr_t_index = None
+				f_next_t_index = None
 				if round(self.car_list[i].t[0],3) <= time:
 					for q in range(0, len(self.car_list[i].t)):
 						if time == round(self.car_list[i].t[q],3):
@@ -121,40 +210,30 @@ class carManager:
 						self.car_list[i].angular_V, self.car_list[i].vel[curr_t_index], self.car_list[i].desired_vel,
 						self.car_list[i].acc[curr_t_index],	self.car_list[i].priority, self.car_list[i].length, self.car_list[i].width,
 						self.car_list[i].max_V,	self.car_list[i].max_A, self.car_list[i].min_A, self.car_list[i].max_lateral_g)
-					if response[0]:
+					if response[0]: 
 						print("Request accepted for Car: ",self.car_list[i].car_id," At time: ",time)
 						self.car_list[i].reservation = np.append(self.car_list[i].reservation[0:curr_t_index], np.ones(len(response[1]),dtype=bool))
-						# self.car_list[i].x = self.car_list[i].x[0:curr_t_index].append(response[1])
 						self.car_list[i].x = np.append(self.car_list[i].x[0:curr_t_index], response[1])
-						# self.car_list[i].y = self.car_list[i].y[0:curr_t_index].append(response[2])
 						self.car_list[i].y = np.append(self.car_list[i].y[0:curr_t_index], response[2])
-						# self.car_list[i].heading = self.car_list[i].heading[0:curr_t_index].append(response[3])
 						self.car_list[i].heading = np.append(self.car_list[i].heading[0:curr_t_index], response[3])
-						# self.car_list[i].vel = self.car_list[i].vel[0:curr_t_index].append(response[4])
 						self.car_list[i].vel = np.append(self.car_list[i].vel[0:curr_t_index], response[4])
-						#self.car_list[i].t
-						#################################################################################################
-						# Not updating the time causes an ArrayIndexOutOfBounds exception when looking at the follow car
-						#################################################################################################
 						self.car_list[i].t = np.append(self.car_list[i].t[0:curr_t_index], response[5])
 					else:
 						print("Request rejected for Car: ",self.car_list[i].car_id," At time: ",time)
-						#################################################################################
-						# I"m not sure why you have this check in here but it appears to always be false
-						# if round(self.car_list[i].t[1],3) <= time:
-						#################################################################################
-						follow_car = None
-						for j in range(i-1, -1, -1):
-							if self.car_list[j].lane_id == self.car_list[i].lane_id: # DEBATE placing "and self.car_list[j].reservation == False 
-								follow_car = self.car_list[j]
-								for u in range(0, len(follow_car.t)):
-									if time == round(follow_car.t[u],3):
-										f_curr_t_index = u
-										break
-								if f_curr_t_index == None:
-									follow_car = None
-								break
-						self.car_list[i]._update(curr_t_index+1, follow_car, f_curr_t_index)
+						if curr_t_index+1 < len(self.car_list[i].t):
+							next_t_index = curr_t_index + 1
+							follow_car = None
+							for j in range(i-1, -1, -1):
+								if self.car_list[j].lane_id == self.car_list[i].lane_id and round(self.car_list[j].t[-1],3) >= time+1: # DEBATE placing "and self.car_list[j].reservation == False 
+									follow_car = self.car_list[j]
+									for u in range(0, len(follow_car.t)):
+										if time == round(follow_car.t[u],3):
+											f_next_t_index = u + 1
+											break
+									if follow_car._check_for_exit(f_next_t_index):
+										follow_car = None
+									break
+							self.car_list[i]._update(next_t_index, follow_car, f_next_t_index)
 				else:
 					break
 
@@ -165,11 +244,7 @@ def car_request_client(car_id, lane_id, t, x, y, heading, angular_V, vel, desire
 	# print(car_id, lane_id, t, x, y, heading, angular_V, vel, acc, priority, length, width, max_V, max_A, min_A, max_lateral_g)
 	try:
 		car_request = rospy.ServiceProxy('car_request', Request)
-		# resp_success, resp_x, resp_y, resp_heading, resp_vel, resp_t = car_request(car_id, int(lane_id), t, x, y, heading, angular_V, vel, acc, priority, length, width, max_V, max_A, min_A, max_lateral_g) 
 		response = car_request(car_id, int(lane_id), t, x, y, heading, angular_V, vel, desired_vel, acc, priority, length, width, max_V, max_A, min_A, max_lateral_g) 
-		# print ("Request has returned ", resp1, " for car_id: ", car_id, ", lane_id: ", lane_id, ", t: ", t, ", x: ", x, ", y:", y, ", heading: ", heading, 
-		# 	", angular_V: ", angular_V, ", vel: ", vel, ", acc: ", acc)
-		# resp = (resp_success, resp_x, resp_y, resp_heading, resp_vel, resp_t)
 		resp = (response.success, response.x, response.y, response.heading, response.vel, response.t)
 		return resp
 	except rospy.ServiceException, e:
@@ -179,18 +254,9 @@ def car_request_client(car_id, lane_id, t, x, y, heading, angular_V, vel, desire
 #-------------------- Functions for generating cars ---------------------------------
 def match_spawn_count(cars_spawned, linearly = True):
 	car_id = 1
-	spawn_vel_range = (17.88, 26.83) # All speeds in meters/sec (effectively (40, 60) mph)
+	spawn_vel_range = (17.88, 20.11) # All speeds in meters/sec (effectively (40, 45) mph)
 	if not linearly:
 		pass
-		# spawn_car_every = 0
-		# for t in np.arange(0, time_to_complete + timestep_size, timestep_size):
-		# 	spawn_car_every = 8*time_to_complete/num_cars
-		# 	if t > 1/8*time_to_complete:
-		# 		spawn_car_every = 4*time_to_complete/num_cars
-		# 	elif t > 1/4*time_to_complete:
-		# 		spawn_car_every = 2*time_to_complete/num_cars
-		# 	elif: t> 1/2*time_to_complete:
-		# 		spawn_car_every = time_to_complete/num_cars
 	else:
 		spawn_car_every = time_to_complete/num_cars
 		for t in np.arange(0, time_to_complete + timestep_size, timestep_size):
@@ -213,7 +279,7 @@ def match_spawn_count(cars_spawned, linearly = True):
 	return cars_spawned
 
 def check_voided_lanes(t, cars_spawned):
-#check if cars in the same lane arent within a certain time tSafe of each other before adding them
+# Check if cars in the same lane arent within a certain time tSafe of each other before adding them
 	lane_set = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
 	free_lanes = lane_set
 	for i in range(1, len(cars_spawned)+1):
@@ -237,15 +303,15 @@ def init_state(t, lane):
 	return x, y, h, vel, acc, following, reservation
 
 def main():
-	warnings.filterwarnings("ignore")
+	#warnings.filterwarnings("ignore")
 	# ----------------------------- Sim configurations ------------------------------
 	np.random.seed(1)
 	global x_states, y_states, h_states, timestep_size, num_cars, tSafe, time_to_complete, end_time, dMax, dSafe, lane_width
 	timestep_size = 0.1 # Must be a float
-	num_cars = 120.0 # Must be a float
-	tSafe = 0.2 # Must be a float
-	time_to_complete = 30.0 # Must be a float
-	end_time = 100.0 # Must be a float
+	num_cars = 100.0 # Must be a float
+	tSafe = 0.5 # Must be a float
+	time_to_complete = 20.0 # Must be a float
+	end_time = 90.0 # Must be a float
 	cars_spawned = []
 	dMax = 148 
 	dSafe = 2
@@ -299,14 +365,18 @@ def main():
 	cm = carManager(cars_spawned)
 	start_time = time.time()
 	sim_time = 0
+	#pdb.set_trace()
 	while not rospy.is_shutdown():
 		cm.update(sim_time)
 		sim_time = sim_time + timestep_size
-		if sim_time >= end_time + timestep_size:
+		if round(sim_time,3) >= end_time + timestep_size:
+			#pdb.set_trace()
 			completion_time = time.time() - start_time
 			print "Simulation Complete \n Execution Time: ", round(completion_time,2), " seconds"
+			# print "Calculating Average Delay..."
+			# print ("Average Delay is: ", cm.calculate_delay)
 			print("Initiating Visualization. Please run Rviz")
-
+			raw_input('Press ENTER to start Visualization')
 			visualizeSim.main(cm.car_list, dMax, lane_width, timestep_size, end_time)
 			print("Visualization complete and shutdown successful!")
 			rospy.signal_shutdown("Simulation Complete")
